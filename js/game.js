39e910ee390c23b2f788b96bfac83e770c62236d
Ludusamo <ludusamo@gmail.com>
2014-12-14 09:20:31 -0500

    File Transfer

---
+++ js/game.js
@@ -0,0 +1,18 @@
+var WIDTH = 800, HEIGHT = 600;
+var game = createBasicGame(WIDTH, HEIGHT, Phaser.CANVAS);
+
+addState('mainMenu', MainMenu, true);
+addState('town', Town);
+addState('arena', Arena);
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
